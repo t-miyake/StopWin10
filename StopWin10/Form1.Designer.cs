9a8272826fd3a4f629184dd2e20de95e59ad7d6d
t-miyake <t@miyake.me>
2016-06-22 23:25:24 +0900

    Initial commit

---
+++ StopWin10/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace StopWin10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 123);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(391, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Windows10にしない (´・ω・｀)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 256);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(391, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Windows10にする (｀・ω・´)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Windows10への自動アップデート：有効";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(422, 398);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ StopWin10/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+
+namespace StopWin10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\Gwx", "DisableGwx", 1);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DisableOSUpgrade", 1);
+            MessageBox.Show("設定が完了しました。");
+            CheckStatus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\Gwx", "DisableGwx", 0);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DisableOSUpgrade", 0);
+            MessageBox.Show("設定が完了しました。");
+            CheckStatus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckStatus();
+        }
+
+        void WriteReg(string SubKey, string KeyName,int Value)
+        {
+            var key = Registry.LocalMachine.CreateSubKey(SubKey);
+            key.SetValue(KeyName, Value, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        int ReadReg(string SubKey, string KeyName)
+        {
+            try
+            {
+                var key = Registry.LocalMachine.OpenSubKey(SubKey);
+                return (int)key.GetValue(KeyName);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        void CheckStatus()
+        {
+            switch (ReadReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DisableOSUpgrade"))
+            {
+                case 0:
+                    label1.Text = "Windows10への自動アップデート：有効";
+                    break;
+                case 1:
+                    label1.Text = "Windows10への自動アップデート：無効";
+                    break;
+                default:
+                    label1.Text = "Windows10への自動アップデート：不明";
+                    break;
+            }
+        }
+
+    }
+}
